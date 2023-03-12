08ae2efd43348c43aac5d8bb2f5d6e79541f6a83
sunnysette <sunnysette@gmail.com>
2023-03-12 18:38:13 +0000

    WIP

---
+++ gatsby-browser.js
@@ -0,0 +1,3 @@
+import "normalize.css";
+import "./src/css/style.scss";
+
--- gatsby-config.js
+++ gatsby-config.js
@@ -3,21 +3,42 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `PKS`,
+    title: `PK Constuctions`,
     siteUrl: `https://www.pkconstructions.uk`
   },
   // "gatsby-plugin-google-gtag",
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        "icon": "src/images/icon.png"
+      }
     },
-    __key: "images"
-  }]
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "images",
+        "path": "./src/images/"
+      },
+      __key: "images"
+    },
+    {
+      resolve: `gatsby-omni-font-loader`,
+      options: {
+        enableListener: true,
+        preconnect: [`https://fonts.googleapis.com`, `https://fonts.gstatic.com`],
+        web: [
+          {
+            name: `Roboto`,
+            file: `https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,700;1,400;1,700&display=swap`,
+          },
+        ],
+      },
+    }
+  ]
 };
